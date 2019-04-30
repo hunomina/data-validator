8b97bb169dc574757d1cb91aac450ef4d1151a53
hunomina <hugo.f972@gmail.com>
2019-04-30 10:34:52 +0200

    JsonData now implements ArrayAccess interface

--- src/Data/JsonData.php
+++ src/Data/JsonData.php
@@ -4,7 +4,7 @@ namespace hunomina\Validator\Json\Data;
 
 use hunomina\Validator\Json\Exception\InvalidDataException;
 
-class JsonData implements DataType
+class JsonData implements DataType, \ArrayAccess
 {
     /**
      * @var array $data
@@ -62,5 +62,70 @@ class JsonData implements DataType
         }
 
         return $jsonData;
+    }
+
+    /**
+     * Whether a offset exists
+     * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset): bool
+    {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link https://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            return $this->data[$offset];
+        }
+        return null;
+    }
+
+    /**
+     * Offset to set
+     * @link https://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value): void
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset): void
+    {
+        unset($this->data[$offset]);
     }
 }
