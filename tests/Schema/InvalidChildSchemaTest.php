b2503b81887d723c58ba7f735759534cb23d127b
hunomina <hugo.f972@gmail.com>
2019-12-29 22:27:41 +0100

    Add test for invalid child schema

---
+++ tests/Schema/InvalidChildSchemaTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace hunomina\Validator\Json\Test\Schema;
+
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use hunomina\Validator\Json\Rule\JsonRule;
+use hunomina\Validator\Json\Schema\JsonSchema;
+use PHPUnit\Framework\TestCase;
+
+class InvalidChildSchemaTest extends TestCase
+{
+    /**
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidChildSchema(): void
+    {
+        $this->expectException(InvalidSchemaException::class);
+        $this->expectExceptionCode(InvalidSchemaException::INVALID_OBJECT_SCHEMA);
+
+        new JsonSchema([
+            'object' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => 'invalid-schema']
+        ]);
+    }
+}
