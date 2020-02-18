99aa69635a327b054ace62c5ccfc6a09fc065e57
hunomina <hugo.f972@gmail.com>
2020-02-18 21:03:09 +0100

    Replace InvalidDataTypeException by InvalidDataTypeArgumentException implementing InvalidArgumentException

---
+++ src/Exception/InvalidDataTypeArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace hunomina\DataValidator\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Class InvalidDataTypeArgumentException
+ * @package hunomina\DataValidator\Exception
+ * Thrown when a schema tries to validate an invalid data type
+ */
+class InvalidDataTypeArgumentException extends InvalidArgumentException
+{
+}
