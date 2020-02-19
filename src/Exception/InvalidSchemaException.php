40ed0c0ecc04a7d7f8141ddc9907b618eebe643c
hunomina <hugo.f972@gmail.com>
2020-02-19 19:05:53 +0100

    Add InvalidSchemaException parent class

---
+++ src/Exception/InvalidSchemaException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace hunomina\DataValidator\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Class InvalidJsonSchemaException
+ * @package hunomina\DataValidator\Exception
+ * Throw when an invalid schema is set
+ */
+class InvalidSchemaException extends InvalidArgumentException
+{
+}
