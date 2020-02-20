3c26245de99c77bc9a8ae43c543d7525f9c64ac2
hunomina <hugo.f972@gmail.com>
2020-02-20 09:52:15 +0100

    Add test for child schema instantiation

--- tests/Schema/Json/InvalidChildSchemaTest.php
+++ tests/Schema/Json/InvalidChildSchemaTest.php
@@ -6,16 +6,33 @@ use hunomina\DataValidator\Exception\Json\InvalidSchemaException;
 use hunomina\DataValidator\Rule\Json\JsonRule;
 use hunomina\DataValidator\Schema\Json\JsonSchema;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 class InvalidChildSchemaTest extends TestCase
 {
-    public function testThrowOnInvalidChildSchema(): void
+    public function testThrowOnNonArrayChildSchema(): void
     {
         $this->expectException(InvalidSchemaException::class);
         $this->expectExceptionCode(InvalidSchemaException::INVALID_CHILD_SCHEMA);
 
         new JsonSchema([
-            'object' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => 'invalid-schema']
+            'object' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => 'invalid-schema'] // not an array
         ]);
+    }
+
+    public function testThrowOnInvalidChildSchema(): void
+    {
+        try {
+            new JsonSchema([
+                'object' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => ['field']] // invalid schema: missing field type
+            ]);
+        } catch (Throwable $t) {
+            $this->assertInstanceOf(InvalidSchemaException::class, $t);
+            $this->assertEquals(InvalidSchemaException::INVALID_CHILD_SCHEMA, $t->getCode());
+
+            $t = $t->getPrevious();
+            $this->assertInstanceOf(InvalidSchemaException::class, $t);
+            $this->assertEquals(InvalidSchemaException::MISSING_TYPE, $t->getCode());
+        }
     }
 }
